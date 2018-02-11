d233cd4218aa6fe5b4b207af8aacdcc89e8d09ea
carlozaul25 <carloz18rivera@live.com>
2018-02-11 04:46:26 -0500

    js

--- Desktop/soundbox/box.js
+++ Desktop/soundbox/box.js
@@ -1,1 +1,11 @@
-console.log('yooo')
+var cNote = document.getElementById('cAudio');
+$('#c').mousedown(function(){
+	cNote.currentTime = 0;
+    cNote.play();
+});
+
+var dNote = document.getElementById('dAudio');
+$('#c').mousedown(function() {
+	dNote.currentTime = 0;
+	dNote.play();
+});
